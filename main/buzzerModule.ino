4888d5ac3b277f71eaddc1c6fcb05e7dc41a32e0
ShailinB <shailinbhadai@hotmail.com>
2021-11-16 15:29:36 +0100

    Added buzzer functionality
    New step is making a statemachine for it.

---
+++ main/buzzerModule.ino
@@ -0,0 +1,60 @@
+
+// Buzzer setup
+const int PIN_BUZZER = 12;
+const int VOLUME = 1000000; //KHz
+
+// Intervals
+const int INTERVAL_TIK_LOPEN = 200;
+const int INTERVAL_STILTE_LOPEN = 1500;
+const int INTERVAL_LOPEN = 750;
+const int INTERVAL_NIET_LOPEN = 1.5 * 1000;//1.5sec
+
+// Timers
+unsigned long buzzerTimer;
+
+// Rhythm variables
+int buzzCount;
+
+void buzzerSetup() {
+  pinMode(PIN_BUZZER, OUTPUT);
+  buzzerTimer = timerSetup(INTERVAL_TIK_LOPEN);
+  resetBuzzerCount();
+}
+
+void doorlopen(int wantedTicks) {
+  if (buzzCount >= wantedTicks) {
+    noTone(PIN_BUZZER);
+    if (timerIsPassed(buzzerTimer, INTERVAL_STILTE_LOPEN)) {
+      resetBuzzerCount();
+      buzzerTimer = timerReset();
+    }
+  }
+  noTone(PIN_BUZZER);
+  if (timerIsPassed(buzzerTimer, INTERVAL_TIK_LOPEN) && buzzCount < wantedTicks) {
+    Serial.print("Tick: ");
+    Serial.println(buzzCount);
+    tone(PIN_BUZZER, VOLUME);
+    buzzCount++;
+    buzzerTimer = timerReset();
+  }
+}
+
+void halt() {
+  noTone(PIN_BUZZER);
+  if (timerIsPassed(buzzerTimer, INTERVAL_NIET_LOPEN)) {
+    tone(PIN_BUZZER, VOLUME);
+    buzzerTimer = timerReset();
+  }
+}
+
+void lopen() {
+  noTone(PIN_BUZZER);
+  if (timerIsPassed(buzzerTimer, INTERVAL_LOPEN)) {
+    tone(PIN_BUZZER, VOLUME);
+    buzzerTimer = timerReset();
+  }
+}
+
+void resetBuzzerCount() {
+  buzzCount = 0;
+}
--- main/main.ino
+++ main/main.ino
@@ -1,9 +1,11 @@
-void setup() {
-  // put your setup code here, to run once:
 
+void setup() {
+  Serial.begin(9600);
+  buzzerSetup();
 }
 
 void loop() {
-  // put your main code here, to run repeatedly:
-
+//  halt();
+//  lopen();
+  doorlopen(4);
 }
