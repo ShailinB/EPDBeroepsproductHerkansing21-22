7684db47c429fafdc9f446ab8f21d4ee6faa6d75
ShailinB <shailinbhadai@hotmail.com>
2021-11-23 15:14:08 +0100

    Added stoplichten

--- main/ledControl.ino
+++ main/ledControl.ino
@@ -1,8 +1,8 @@
 
 // Pin diagram stoplichten -------------------------------
 
-const int LEDPINS [] = {13, 12, 8, 7, A5, A4};
 const int LEDPINS_SIZE = 6;
+const int LEDPINS [] = {13, 8, 7, 4, 3, 2};
 
 void ledControlSetup() {
   for (int i = 0; i < LEDPINS_SIZE; i++) {
@@ -36,4 +36,32 @@ void ledControlSetLedOff (int led) {
       digitalWrite(LEDPINS[i], LOW);
     }
   }
+}
+
+// Getters
+
+//Linker stoplicht
+const int getLinker_RoodLicht() {
+  return 13;
+}
+
+const int getLinker_OranjeLicht() {
+  return 8;
+}
+
+const int getLinker_GroenLicht() {
+  return 7;
+}
+
+//Rechter stoplicht
+const int getRechter_RoodLicht() {
+  return 4;
+}
+
+const int getRechter_OranjeLicht() {
+  return 3;
+}
+
+const int getRechter_GroenLicht() {
+  return 2;
 }
